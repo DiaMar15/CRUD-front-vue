c10df48f8195395ce8b4c6dd4f063ae5f76bc9bd
Jonatan andres <jonatan25sanchez@gmail.com>
2025-05-16 11:22:37 -0500

    actualizando rutas de inicio de usuarios

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3333/api/v1/auth';
+const API_URL = 'http://localhost:3333/api/v1';
 
 export const loginRequest = async (correo: string, contrasena: string) => {
-    const response = await axios.post(`${API_URL}/login`, { correo,contrasena,});
+    const response = await axios.post(`${API_URL}/login`, { correo, contrasena,});
     return response.data;
 }
 
