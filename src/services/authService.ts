a854e206c281099746628d7a6c17c5fadb9f906c
Jonatan andres <jonatan25sanchez@gmail.com>
2025-05-15 20:52:41 -0500

    usando buenas practicas y creando carpeta de services 3

---
+++ src/services/authService.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_URL = 'http://localhost:3333/api/v1/auth';
+
+export const loginRequest = async (correo: string, contrasena: string) => {
+    const response = await axios.post(`${API_URL}/login`, { correo,contrasena,});
+    return response.data;
+}
+
+export const registerRequest = async (nombre: string, correo: string, contrasena: string) => {
+    const response = await axios.post(`${API_URL}/register`, { nombre, correo, contrasena });
+    return response.data;
+}
